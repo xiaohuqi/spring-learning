e4229d0fc90955c121a84c552c39ea8d5aced169
xiaohuqi <xiaohuqi@126.com>
2017-02-05 15:00:14 +0800

    Initial commit

---
+++ ch9_3_4-spring-jms/src/main/java/com/data0123/App.java
@@ -0,0 +1,13 @@
+package com.data0123;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
